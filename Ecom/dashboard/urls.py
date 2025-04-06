fd3059f79a295b96feb1e2c17b9788ddbf13908e
prabeshnpl <nepalprabesh99@gmail.com>
2025-04-06 11:28:27 +0545

    added pagination function to return JsonResponse for loading poducts dynamically.

--- Ecom/dashboard/urls.py
+++ Ecom/dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('products/',views.load_products,name='products'),
 ]
--- Ecom/dashboard/views.py
+++ Ecom/dashboard/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from .models import CustomUser, Cart, CartProduct, Product
+from django.http import JsonResponse
 # Create your views here.
 def home(request):
-    return render(request,'dashboard.html')
+    return render(request, 'dashboard.html')
+
+def load_products(request):
+    page_no = request.GET.get('page',1) #if no pageno default=1
+    paginator = Paginator(Product.objects.all(),20)
+    page = paginator.get_page(page_no)
+
+    products = list(page.object_list.values('id','name','image','price','description'))
+    return JsonResponse({'products':products,'has_next':page.has_next()})
+
+
